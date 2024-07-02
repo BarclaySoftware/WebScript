8c614aa831b76ab389c4098c0381252dd2de7b9a
Jupiter Group <ourjupitergroup@gmail.com>
2024-07-02 13:07:25 -0400

    Release 0.4

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ editor.setTheme("ace/theme/monokai");
 editor.session.setMode("ace/mode/html");
 editor.setOptions({
     fontSize: "16px",
-    fontFamily: 'monospace',
+    fontFamily: 'Courier Now AT',
     wrap: false,
     showPrintMargin: false,
     enableBasicAutocompletion: true,
